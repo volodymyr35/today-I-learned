dc6297dc4fac42bba2da4fcb78d46063e4a9a268
volodymyr35 <vzhukevych@gmail.com>
2024-09-11 15:52:07 +0300

    fixed project security issue

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://lxnufytjhrrdgrdkwtrs.supabase.co';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4bnVmeXRqaHJyZGdyZGt3dHJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUwMzE2MzUsImV4cCI6MjA0MDYwNzYzNX0.igx37Q1Yolo0POEGZEfSE790irryyVl0v87866w9-p4';
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
